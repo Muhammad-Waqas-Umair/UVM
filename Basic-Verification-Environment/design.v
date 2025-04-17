a58e3318a881c3606d1f0e79b1af19ad4d1e67f2
Muhammad Waqas Umair <waqasumair111@gmail.com>
2025-04-17 16:42:02 +0200

    Update design.v

--- Basic-Verification-Environment/design.v
+++ Basic-Verification-Environment/design.v
@@ -1,22 +1,24 @@
+// ---------------------- D FLIP-FLOP DESIGN -------------------------
 module d_ff (
-    input  logic clock,
-    input  logic reset,
-    input  logic data_in,
-    output reg   data_out
+    input clock,
+    input reset,       
+    input data_in,
+    output reg data_out
 );
-    always @(posedge clock) begin
+
+    always @(posedge clock or posedge reset) begin
         if (reset) begin
-            data_out <= 1'b0;
-        end
-        else begin
-            data_out <= data_in;
+            data_out <= 1'b0;  
+        end else begin
+            data_out <= data_in;  
         end
     end
-endmodule: d_ff
+endmodule
 
-interface dff_interface;
+// ---------------------- DFF INTERFACE -------------------------
+interface dff_interface();
     logic clock;
     logic reset;
     logic data_in;
     logic data_out;
-endinterface: dff_interface
+endinterface
